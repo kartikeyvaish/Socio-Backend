34734f8b26ca29a2945713a602764e301b661bf0
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-08-20 23:58:55 +0530

    Created Mail sending Helper File

---
+++ config/Mailer.js
@@ -0,0 +1,38 @@
+const Email = require("email-templates");
+const nodemailer = require("nodemailer");
+
+const config = require("../config/Configurations");
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.email,
+    pass: process.env.password,
+  },
+});
+
+const Mailer = new Email({
+  transport: transporter,
+  preview: false,
+  send: true,
+});
+
+async function SendOTPEmail({ to = "", subject = "", locals = {} }) {
+  try {
+    const response = await Mailer.send({
+      template: "OTP",
+      message: {
+        to: to,
+        from: `Socio <${process.env.email}>`,
+        subject: subject,
+      },
+      locals: locals,
+    });
+
+    return { response: "Email Sent Successfully", ok: true, data: response };
+  } catch (error) {
+    return { response: config.messages.serverError, ok: false };
+  }
+}
+
+exports.SendOTPEmail = SendOTPEmail;
