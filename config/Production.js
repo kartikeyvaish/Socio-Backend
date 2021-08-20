1cc8f42dcbb223521bf948906f29d99de2232309
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-08-20 23:57:59 +0530

    Changed Size Limit for Testing

--- config/Production.js
+++ config/Production.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
   app.use(Morgan("dev")); // Delete this line while deployement
   app.use(helmet());
   app.use(compression());
-  app.use(express.json({ limit: "10mb" }));
+  app.use(express.json({ limit: "50mb" }));
   app.use(cors());
   app.use(express.urlencoded({ extended: true }));
 };
