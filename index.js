20d2aeb6dba99db885075be5954e18c8d578ae56
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-08-21 02:07:54 +0530

    Added Posts route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const config = require("./config/Configurations");
 
+const Posts = require("./routes/Posts");
 const User = require("./routes/Users");
 
 const db_url = `mongodb://${process.env.host}:${process.env.DB_PORT}/${process.env.DB_Name}`;
@@ -19,6 +20,7 @@ require("./config/Production")(app);
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(process.env.apiVersion + process.env.auth, User);
+app.use(process.env.apiVersion + process.env.posts, Posts);
 
 app.get("*", (req, res) => {
   res.status(404).sendFile(__dirname + "/views/404.html");
