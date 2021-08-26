6b8a50ece2868de1e374e36ce363f6e4aeccbc81
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-08-27 01:03:19 +0530

    Added Socket.io chat service

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config();
 
+const http = require("http");
+const socketio = require("socket.io");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+
 const config = require("./config/Configurations");
 
 const Chats = require("./routes/Chats");
@@ -24,6 +33,45 @@ mongoose
 const app = express();
 require("./config/Production")(app);
 app.use(express.static(path.join(__dirname, "/public")));
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", msg);
+  });
+
+  socket.on("type-update", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("type-update-emitter", msg);
+  });
+
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
 
 app.use(process.env.apiVersion + process.env.auth, User);
 app.use(process.env.apiVersion + process.env.chats, Chats);
