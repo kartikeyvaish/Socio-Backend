442ff602c4cbbb3d541d566d05c03cc6dbb909f3
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-08-27 01:02:17 +0530

    Added Notifications/Requests/PushNotifications

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const config = require("./config/Configurations");
 
 const Chats = require("./routes/Chats");
 const Comments = require("./routes/Comments");
+const Notifications = require("./routes/Notifications");
 const Likes = require("./routes/Likes");
+const PushNotifications = require("./routes/PushNotifications");
 const Posts = require("./routes/Posts");
 const Profile = require("./routes/Profile");
+const OTP = require("./routes/OTP");
+const Requests = require("./routes/Requests");
 const User = require("./routes/Users");
 
 mongoose
@@ -26,8 +30,14 @@ app.use(process.env.apiVersion + process.env.chats, Chats);
 app.use(process.env.apiVersion + process.env.comments, Comments);
 app.use(process.env.apiVersion + process.env.otps, OTP);
 app.use(process.env.apiVersion + process.env.likes, Likes);
+app.use(process.env.apiVersion + process.env.notifications, Notifications);
 app.use(process.env.apiVersion + process.env.posts, Posts);
+app.use(
+  process.env.apiVersion + process.env.pushnotifications,
+  PushNotifications
+);
 app.use(process.env.apiVersion + process.env.profile, Profile);
+app.use(process.env.apiVersion + process.env.requests, Requests);
 
 app.get("*", (req, res) => {
   res.status(404).sendFile(__dirname + "/views/404.html");
