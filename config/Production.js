172dec0cfbdfd90858a6189099af06ff40fd14c4
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-11-10 12:08:49 +0530

    :zap: changed file upload limit

--- config/Production.js
+++ config/Production.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
   }
   app.use(helmet());
   app.use(compression());
-  app.use(express.json({ limit: "50mb" }));
+  app.use(express.json({ limit: "10mb" }));
   app.use(cors());
   app.use(express.urlencoded({ extended: true }));
 };
