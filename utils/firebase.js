34575aff6bf6141f4cb8f9976f280a9ae256c5a0
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-11-25 13:36:32 +0530

    :bug: FIXED Private key not being parsed

--- utils/firebase.js
+++ utils/firebase.js
@@ -4,7 +4,7 @@ const Firebase = admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.projectId,
     clientEmail: process.env.clientEmail,
-    privateKey: process.env.privateKey,
+    privateKey: process.env.privateKey.replace(/\\n/g, "\n"),
   }),
 });
 
